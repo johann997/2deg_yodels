2df4fb65a61bf5833f1e2ad30e126e30816bfeb0
johann997 <77413531+johann997@users.noreply.github.com>
2023-04-14 15:12:20 -0700

    change environment name

--- setup.sh
+++ setup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-
 # create environment
-conda create  --name 2deg_yodel
-conda activate 2deg_yodel
+conda create  --name 2deg_yodels_env
+conda activate 2deg_yodels_env
 conda install -c anaconda python
-
-
 
 # Install required packages using pip
 pip install -r requirements.txt
