08647b9dbc89fb7ebe1b163492690e0e3b98d921
johann997 <77413531+johann997@users.noreply.github.com>
2023-04-14 15:35:51 -0700

    Add comments when setuo.sh is run

--- setup.sh
+++ setup.sh
@@ -2,9 +2,22 @@
 
 # create environment
 conda create  --name 2deg_yodels_env
+printf %"$COLUMNS"s |tr " " "-"
+echo "Environment Created"
+printf %"$COLUMNS"s |tr " " "-"
+
 conda activate 2deg_yodels_env
+printf %"$COLUMNS"s |tr " " "-"
+echo "Environment Activated"
+printf %"$COLUMNS"s |tr " " "-"
+
 conda install -c anaconda python
+printf %"$COLUMNS"s |tr " " "-"
+echo "Python Installed"
+printf %"$COLUMNS"s |tr " " "-"
 
 # Install required packages using pip
 pip install -r requirements.txt
-
+printf %"$COLUMNS"s |tr " " "-"
+echo "Packages Installed"
+printf %"$COLUMNS"s |tr " " "-"
