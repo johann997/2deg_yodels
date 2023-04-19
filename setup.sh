6a316860cdd219118a7c25ad5f7c9862489316cd
johann997 <77413531+johann997@users.noreply.github.com>
2023-04-19 10:24:16 -0700

    adding kwant via conda install

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "STARTING SETUP"
+printf %"$COLUMNS"s |tr " " "-"
+printf %"$COLUMNS"s |tr " " "-"
+
 # create environment
 conda create  --name 2deg_yodels_env
 printf %"$COLUMNS"s |tr " " "-"
@@ -21,3 +25,15 @@ pip install -r requirements.txt
 printf %"$COLUMNS"s |tr " " "-"
 echo "Packages Installed"
 printf %"$COLUMNS"s |tr " " "-"
+
+
+# Install kwant (using pip need to manually install MUMPS)
+conda install kwant -c conda-forge
+printf %"$COLUMNS"s |tr " " "-"
+echo "kwant installed"
+printf %"$COLUMNS"s |tr " " "-"
+
+
+printf %"$COLUMNS"s |tr " " "-"
+printf %"$COLUMNS"s |tr " " "-"
+echo "FINISHED SETUP"
