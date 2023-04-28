694fd31ed3d7f104dd9239fc6fdae87a7d54892f
johann997 <johann@pdrayne.com>
2023-04-28 16:55:35 -0700

    install kwant first in the setup.sh file

--- setup.sh
+++ setup.sh
@@ -20,19 +20,17 @@ printf %"$COLUMNS"s |tr " " "-"
 echo "Python Installed"
 printf %"$COLUMNS"s |tr " " "-"
 
-# Install required packages using pip :: pip freeze > requirements.txt
-pip install -r requirements.txt
-printf %"$COLUMNS"s |tr " " "-"
-echo "Packages Installed"
-printf %"$COLUMNS"s |tr " " "-"
-
-
 # Install kwant (using pip need to manually install MUMPS)
 conda install kwant -c conda-forge
 printf %"$COLUMNS"s |tr " " "-"
 echo "kwant installed"
 printf %"$COLUMNS"s |tr " " "-"
 
+# Install required packages using pip :: pip freeze > requirements.txt
+pip install -r requirements.txt
+printf %"$COLUMNS"s |tr " " "-"
+echo "Packages Installed"
+printf %"$COLUMNS"s |tr " " "-"
 
 printf %"$COLUMNS"s |tr " " "-"
 printf %"$COLUMNS"s |tr " " "-"
